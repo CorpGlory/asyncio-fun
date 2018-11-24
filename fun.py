255edf11008ac7e77ede5b9fcb243c3e2f01e7b7
Alexey Velikiy <av@corpglory.com>
2018-11-24 22:26:51 +0300

    step 4

--- fun.py
+++ fun.py
@@ -11,8 +11,8 @@ async def do_async_sleep(i: str, n: int):
 
 def execution_job(i: str, n: int):
     print("Start exec", i)
-    res = pow(7, n)
-    print("End exec", i, ':', res)
+    res = pow(100, n * 10000)
+    print("End exec", i, ':', n)
 
 def input_reader_loop(callback_loop: asyncio.AbstractEventLoop, executor: Executor):
     task_number = 0
@@ -27,22 +27,19 @@ def input_reader_loop(callback_loop: asyncio.AbstractEventLoop, executor: Execut
             continue
 
         n = int(splits[1])
+
         if splits[0] == 's':
-            coro = do_async_sleep(task_number, n)
+            print('Send sleep task...', task_number)
+            asyncio.run_coroutine_threadsafe(do_async_sleep(task_number, n), loop=callback_loop)
 
         if splits[0] == 'x':
-            coro = loop.run_in_executor(executor, execution_job, task_number, n)
-
-        if coro is None:
-            print('Bad task...')
-            continue
-
-        print('Send task...', splits[0], task_number)
-        asyncio.run_coroutine_threadsafe(coro, loop=callback_loop)
+            print('Send compute task...', task_number)
+            loop.run_in_executor(executor, execution_job, task_number, n)
 
 
 if __name__ == "__main__":
     print("Start")
     loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
     executor = ThreadPoolExecutor(max_workers=None)
-    loop.run_until_complete(loop.run_in_executor(None, input_reader_loop, loop, executor))
+    loop.run_in_executor(executor, input_reader_loop, loop, executor)
+    loop.run_forever()
