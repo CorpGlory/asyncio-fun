947a526f31aa9209c07e03d4ca9dcb3b5a7ec33f
Alexey Velikiy <av@corpglory.com>
2018-11-24 21:56:24 +0300

    step 3

--- fun.py
+++ fun.py
@@ -1,28 +1,48 @@
 import asyncio
 import sys
+from concurrent.futures import Executor, ThreadPoolExecutor
 
 
-print(sys.version)
-
-async def doit(i: str, s: int):
-    print("Start %d" % i)
-    await asyncio.sleep(s)
-    print("End %d" % i)
+async def do_async_sleep(i: str, n: int):
+    print("Start sleep", i)
+    await asyncio.sleep(n)
+    print("End sleep", i)
     return i
 
+def execution_job(i: str, n: int):
+    print("Start exec", i)
+    res = pow(7, n)
+    print("End exec", i, ':', res)
 
-def input_reader_loop(callback_loop: asyncio.AbstractEventLoop):
+def input_reader_loop(callback_loop: asyncio.AbstractEventLoop, executor: Executor):
     task_number = 0
     while True:
         task_number = task_number + 1
-        s = int(sys.stdin.readline())
-        asyncio.run_coroutine_threadsafe(doit(task_number, s), loop=callback_loop)
-        asyncio.sleep(1)
-        print('Sent task', task_number)
+        line: str = sys.stdin.readline()
+        splits = line.split(' ')
+
+        coro = None
+        if len(splits) < 2:
+            print('...')
+            continue
+
+        n = int(splits[1])
+        if splits[0] == 's':
+            coro = do_async_sleep(task_number, n)
+
+        if splits[0] == 'x':
+            coro = loop.run_in_executor(executor, execution_job, task_number, n)
+
+        if coro is None:
+            print('Bad task...')
+            continue
+
+        print('Send task...', splits[0], task_number)
+        asyncio.run_coroutine_threadsafe(coro, loop=callback_loop)
 
 
 if __name__ == "__main__":
     print("Start")
     loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
-    loop.run_until_complete(loop.run_in_executor(None, input_reader_loop, loop))
-    print('oops')
+    executor = ThreadPoolExecutor(max_workers=None)
+    loop.run_until_complete(loop.run_in_executor(None, input_reader_loop, loop, executor))
